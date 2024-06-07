a46a83ff7e90c51d4091117f6d2f3664a7347c7c
Jonas Otter <jonas.otter@winfinity.live>
2024-06-07 16:27:14 +0500

    feat: service

--- cert.sh
+++ cert.sh
@@ -5,4 +5,6 @@ if [ ! -d "./certs" ]; then
   mkdir -p "./certs"
 fi
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/selfsigned.key -out ./certs/selfsigned.crt
+source .env
+
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/C=NL/ST=North Holland/L=Amsterdam/O=Serices/OU=Ping/CN=${NETWORK_DOMAIN}"
